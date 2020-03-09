9012d5f5b9313b3540bab89fa60d63839e73f86a
Florian Bollmann <florian@gentlent.com>
2020-03-09 13:43:23 +0100

    build a calculator

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+function nummer0(){
+  if (document.getElementById("output").innerHTML == 0){
+    document.getElementById("output").innerHTML = 0;
+  } else {document.getElementById("output").innerHTML += 0;}
+}
+
+function nummer1(){
+  if (document.getElementById("output").innerHTML == 0){
+    document.getElementById("output").innerHTML = 1;
+  } else {document.getElementById("output").innerHTML += 1;}
+}
+
+function nummer2(){
+  if (document.getElementById("output").innerHTML == 0){
+    document.getElementById("output").innerHTML = 2;
+  } else {document.getElementById("output").innerHTML += 2;}
+}
+
+function nummer3(){
+  if (document.getElementById("output").innerHTML == 0){
+    document.getElementById("output").innerHTML = 3;
+  } else {document.getElementById("output").innerHTML += 3;}
+}
+
+function nummer4(){
+  if (document.getElementById("output").innerHTML == 0){
+    document.getElementById("output").innerHTML = 4;
+  } else {document.getElementById("output").innerHTML += 4;}
+}
+
+function nummer5(){
+  if (document.getElementById("output").innerHTML == 0){
+    document.getElementById("output").innerHTML = 5;
+  } else {document.getElementById("output").innerHTML += 5;}
+}
+
+function nummer6(){
+  if (document.getElementById("output").innerHTML == 0){
+    document.getElementById("output").innerHTML = 6;
+  } else {document.getElementById("output").innerHTML += 6;}
+}
+
+function nummer7(){
+  if (document.getElementById("output").innerHTML == 0){
+    document.getElementById("output").innerHTML = 7;
+  } else {document.getElementById("output").innerHTML += 7;}
+}
+
+function nummer8(){
+  if (document.getElementById("output").innerHTML == 0){
+    document.getElementById("output").innerHTML = 8;
+  } else {document.getElementById("output").innerHTML += 8;}
+}
+
+function nummer9(){
+  if (document.getElementById("output").innerHTML == 0){
+    document.getElementById("output").innerHTML = 9;
+  } else {document.getElementById("output").innerHTML += 9;}
+}
+
+
+function plus(){
+  document.getElementById("output").innerHTML += "+";
+}
+
+function minus(){
+  document.getElementById("output").innerHTML += "-";
+}
+
+function mal(){
+  document.getElementById("output").innerHTML += "*";
+}
+
+function geteilt(){
+  document.getElementById("output").innerHTML += "/";
+}
+
+
+function Istgleich(){
+  document.getElementById("output").innerHTML = eval(document.getElementById("output").innerHTML);
+}
